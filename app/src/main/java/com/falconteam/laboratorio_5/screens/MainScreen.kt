6de6da7396b7f4bc49748688dbf4c590a5a5c2f7
Marroquin02 <00025221@uca.edu.sv>
2024-05-20 16:16:28 -0600

    Add: Estructura de archivos y vistas

---
+++ app/src/main/java/com/falconteam/laboratorio_5/screens/MainScreen.kt
@@ -0,0 +1,13 @@
+package com.falconteam.laboratorio_5.ui.screens
+
+import androidx.compose.runtime.Composable
+import com.falconteam.laboratorio_5.utils.BlogsitoViewModel
+
+
+@Composable
+fun MainScreen(
+    viewModel: BlogsitoViewModel,
+    onClick: () -> Unit
+) {
+
+}
